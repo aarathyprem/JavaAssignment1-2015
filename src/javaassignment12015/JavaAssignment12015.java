91106d8d7749545a2d3333a0ee3bbfc67a3cfc48
c0660563 <c0660563@SB-N208-10.lambton.on.ca>
2015-09-14 15:53:03 -0400

    Answered questions

---
+++ src/javaassignment12015/JavaAssignment12015.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaassignment12015;
+
+/**
+ *
+ * @author c0660563
+ */
+public class JavaAssignment12015 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+        
+        /*************************************
+         * Job Posting : Java Developer
+         * http://ca.indeed.com/cmp/Tundra-Technical-Solution/jobs/Back-End-Java-Developer-be6b886816880859?q=java
+         * 
+         * Java EE - Java Enterprise Edition - https://en.wikipedia.org/wiki/Java_Platform,_Enterprise_Edition
+         * Spring Framework - Application Framework - https://en.wikipedia.org/wiki/Spring_Framework
+         * PIG - High level Map Reduce - https://en.wikipedia.org/wiki/Pig_(programming_tool)
+         * 
+         * Toronto Salary - $78k
+         * Vancouver Salary - $82k
+         * Waterloo Salary - $80k
+         */
+    }
+    
+}
